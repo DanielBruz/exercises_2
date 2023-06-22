c5b35c2acdc27b0f4556484284e1031e3cfdaef5
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-06-22 12:03:08 +0200

    Prvni commit Cykly pole

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+			// Nacteni hlavicky
+			include 'header.php';
+			
+			// Cykly pole
+
+			$badass = 'Brienne of Tarth';
+
+			$arr = array(
+				"Princes Bubblegum",
+				"Imperator Furiosa",
+				$badass,
+				"Bulbasaur"
+			);
+
+			echo '<pre>';
+			print_r ( $arr );
+			echo '</pre>';
+
+			foreach ($arr as $value) {
+				
+
+
+
+			}
+
+
+
+
+			echo '<br>----<br>';
+
+		
+		?>
